1695cc528535d8a2ebb6534009ee8249de23ab6f
Emma Kelly <ejkellyb@icloud.com>
2026-04-29 16:20:30 +0000

    add functionality to search button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonIcon, IonButton } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
+import { MyDataService } from '../services/my-data-service';
 import { addIcons } from 'ionicons';
 import { heartOutline } from 'ionicons/icons'
 
@@ -7,11 +10,17 @@ import { heartOutline } from 'ionicons/icons'
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonIcon, IonButton],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonIcon, IonButton, FormsModule],
 })
 export class HomePage {
   studentNum:string = "G00474347";
-  constructor() {
+  keyword:string = "";
+  constructor(private mds:MyDataService, private router:Router) {
     addIcons({heartOutline})
+  }
+
+  async searchMovies(){
+    await this.mds.set("keyword", this.keyword);
+    this.router.navigate(['/movies'])
   }
 }
