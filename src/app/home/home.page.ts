47f9155695579cd639dc3361b707f5d1be2f557d
Emma Kelly <ejkellyb@icloud.com>
2026-04-29 14:57:31 +0000

    add heart icon to toolbar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonIcon, IonButton } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { heartOutline } from 'ionicons/icons'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonIcon, IonButton],
 })
 export class HomePage {
   studentNum:string = "G00474347";
-  constructor() {}
+  constructor() {
+    addIcons({heartOutline})
+  }
 }
