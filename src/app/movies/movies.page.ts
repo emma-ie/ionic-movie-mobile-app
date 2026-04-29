b3556b29be8483e099433f385bf1594d5400f91d
Emma Kelly <ejkellyb@icloud.com>
2026-04-29 19:36:56 +0000

    set up http service

--- src/app/movies/movies.page.ts
+++ src/app/movies/movies.page.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonCardSubtitle, IonCardContent, IonCard, IonCardHeader } from '@ionic/angular/standalone';
 import { MyDataService } from '../services/my-data-service';
+import { MyHttpService } from '../services/my-http-service';
+import { HttpOptions } from '@capacitor/core';
 
 @Component({
   selector: 'app-movies',
@@ -13,8 +15,12 @@ import { MyDataService } from '../services/my-data-service';
 })
 export class MoviesPage implements OnInit {
 
-  constructor(private mds:MyDataService) { }
+  constructor(private mds:MyDataService, private mhs:MyHttpService) { }
   keyword:string = "";
+  apiKey:string = "dbe32932";
+  options: HttpOptions = {
+    url: "http://www.omdbapi.com/?apikey=" + this.apiKey + "&s=" + this.keyword
+  }
 
   ngOnInit() {
     this.getKeyword();
@@ -22,6 +28,7 @@ export class MoviesPage implements OnInit {
 
   async getKeyword(){
     this.keyword = await this.mds.get("keyword");
+    this.mhs.get(this.options);
   }
 
 }
