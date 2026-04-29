8b01974edf00ba91b31d92004d01d82ca3025c51
Emma Kelly <ejkellyb@icloud.com>
2026-04-29 16:29:19 +0000

    add html to movies and add methods to retrieve value from storage

--- src/app/movies/movies.page.ts
+++ src/app/movies/movies.page.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCardSubtitle, IonCardContent, IonCard, IonCardHeader } from '@ionic/angular/standalone';
+import { MyDataService } from '../services/my-data-service';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.page.html',
   styleUrls: ['./movies.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCardSubtitle, IonCardContent, IonCard, IonCardHeader]
 })
 export class MoviesPage implements OnInit {
 
-  constructor() { }
+  constructor(private mds:MyDataService) { }
+  keyword:string = "";
 
   ngOnInit() {
+    this.getKeyword();
+  }
+
+  async getKeyword(){
+    this.keyword = await this.mds.get("keyword");
   }
 
 }
