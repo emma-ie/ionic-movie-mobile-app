bf927750012a5d77015f3dab6e40696b503bd35b
Emma Kelly <ejkellyb@icloud.com>
2026-04-30 14:17:09 +0000

    show movie data on movies page based on search result

--- src/app/movies/movies.page.ts
+++ src/app/movies/movies.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCardSubtitle, IonCardContent, IonCard, IonCardHeader } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCardSubtitle, IonCardContent, IonCard, IonCardHeader, IonCardTitle } from '@ionic/angular/standalone';
 import { MyDataService } from '../services/my-data-service';
 import { MyHttpService } from '../services/my-http-service';
 import { HttpOptions } from '@capacitor/core';
@@ -11,24 +11,30 @@ import { HttpOptions } from '@capacitor/core';
   templateUrl: './movies.page.html',
   styleUrls: ['./movies.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCardSubtitle, IonCardContent, IonCard, IonCardHeader]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCardSubtitle, IonCardContent, IonCard, IonCardHeader, IonCardTitle]
 })
 export class MoviesPage implements OnInit {
 
-  constructor(private mds:MyDataService, private mhs:MyHttpService) { }
   keyword:string = "";
-  apiKey:string = "dbe32932";
-  options: HttpOptions = {
-    url: "http://www.omdbapi.com/?apikey=" + this.apiKey + "&s=" + this.keyword
-  }
-
+  movies:any[] = [];
+  apiKey:string = "71a8936961cc7f72bb39f09894041612";
+  
+  constructor(private mds:MyDataService, private mhs:MyHttpService) { }
+  
   ngOnInit() {
     this.getKeyword();
   }
-
+  
   async getKeyword(){
     this.keyword = await this.mds.get("keyword");
-    this.mhs.get(this.options);
+
+    const options: HttpOptions = {
+      url: "https://api.themoviedb.org/3/search/movie?query=" + this.keyword + "&api_key=" + this.apiKey
+    }
+
+    let result = await this.mhs.get(options);
+    this.movies = result.data.results;
+    console.log(this.movies);
   }
 
 }
